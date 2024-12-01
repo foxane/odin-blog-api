6e2deb2a574e56050a4dff80b640eeb90f05892f
foxane <65512187+foxane@users.noreply.github.com>
2024-12-01 13:30:09 +0700

    Added author page

--- frontend-admin/src/pages/authorPage/Author.jsx
+++ frontend-admin/src/pages/authorPage/Author.jsx
@@ -1,14 +1,35 @@
-import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import PostCard from './PostCard';
 
-import { AuthContext } from '../../AuthProvider';
+const posts = [
+  {
+    title: 'Long blog post title about something lorem ipsum dolor sit ame',
+    categories: ['cat', 'cat', 'cat'],
+    publishedAt: '',
+    commentCount: 10,
+  },
+  {
+    title: 'Long blog post title about something',
+    categories: ['cat', 'cat', 'cat'],
+    publishedAt: '2023-13-02',
+    commentCount: 0,
+  },
+  {
+    title: 'Long blog post title about something',
+    categories: ['cat', 'cat', 'cat'],
+    publishedAt: '2023-13-02',
+    commentCount: 0,
+  },
+];
 
 export default function Author() {
-  const { user } = useContext(AuthContext);
-
-  if (!user) {
-    return <Navigate to="/login" />;
-  }
-
-  return <p className="text-3xl">Author page placeholder</p>;
+  return (
+    <div className="max-w-screen-xl p-2 mx-auto">
+      <p className="text-xl text-center font-semibold py-2">All posts</p>
+      <div className="grid grid-flow-row gap-3 md:grid-cols-2">
+        {posts.map(p => (
+          <PostCard key={p.title} post={p} />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ frontend-admin/src/pages/authorPage/PostCard.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+
+import editIcon from '../../assets/edit.svg';
+import trashIcon from '../../assets/trash.svg';
+
+export default function PostCard({ post }) {
+  return (
+    <>
+      <div className="flex flex-col sm:grid sm:grid-cols-[_1fr,30px] rounded-md shadow-md shadow-gray-00 p-4">
+        <div className="">
+          <h5 className="font-semibold text-lg mb-2">{post.title}</h5>
+          <p className="text-sm text-neutral-800">
+            Categories:
+            <span className="font-semibold italic ps-2">
+              {post.categories.join(', ')}
+            </span>
+          </p>
+          <p className="text-sm text-neutral-800">
+            Published at:
+            <span className="font-semibold italic p-2">
+              {post.publishedAt ? post.publishedAt : 'Unpublished'}
+            </span>
+          </p>
+          <p className="text-sm text-neutral-800">
+            Comments:
+            <span className="font-semibold italic p-2">
+              {post.commentCount}
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-row mt-2 sm:flex-col gap-1 sm:w-8">
+          <button className="rounded-md p-1 border border-gray-200 hover:bg-slate-200">
+            <img src={editIcon} alt="edit icon" className="w-6" />
+          </button>
+          <button className="rounded-md p-1 border border-gray-200 hover:bg-slate-200">
+            <img src={trashIcon} alt="trash icon" className="w-6" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+PostCard.propTypes = {
+  post: PropTypes.object,
+};
