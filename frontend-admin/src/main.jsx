2c40d92a6fa61316e1cccc2663bb07952c4650d8
foxane <65512187+foxane@users.noreply.github.com>
2024-12-09 14:55:15 +0700

    Change favicon and title

--- frontend-admin/src/main.jsx
+++ frontend-admin/src/main.jsx
@@ -4,6 +4,7 @@ import './styles/index.css';
 import { RouterProvider } from 'react-router-dom';
 import routes from './routes';
 import { AuthProvider } from './AuthProvider';
+if (!import.meta.env.VITE_API_URL) throw new Error('API url not defined');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
