977a1ed1b03bd64cca820719412f0f615e3ffd25
foxane <65512187+foxane@users.noreply.github.com>
2024-12-09 13:53:04 +0700

    Add publish comment

--- frontend-public/src/components/Comment.tsx
+++ frontend-public/src/components/Comment.tsx
@@ -1,27 +1,56 @@
-import { useParams } from 'react-router-dom';
-import useFetch from '../hooks/useFetch';
-import { Entity } from './PostCard';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useState } from 'react';
+
 import { formatDate } from '../utils/utils';
+import { Entity } from './PostCard';
+import useFetch from '../hooks/useFetch';
 import useUser from '../hooks/useUser';
+import axios from 'axios';
 
 export default function Comment() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const {
     data: comments,
     loading,
     error,
   } = useFetch<Com[]>(`/posts/${id || ''}/comments`);
   const { user } = useUser();
+  const [comment, setComment] = useState('');
+
+  const submitComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    axios
+      .post(
+        `${import.meta.env.VITE_API_URL}/comments?postId=${id || ''}`,
+        {
+          content: comment,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token') || ''}`,
+          },
+        },
+      )
+      .then(() => navigate(0))
+      .catch(() => navigate(0)); // Placeholder for error, i'll come back later. maybe
+  };
 
   return (
     <div className="p-2 mt-10">
       <p className="text-lg font-semibold">Comments</p>
 
-      <form className="mt-3 p-2 flex flex-col gap-3 border border-neutral-300 rounded">
+      <form
+        onSubmit={submitComment}
+        className="mt-3 p-2 flex flex-col gap-3 border border-neutral-300 rounded">
         {user ? (
           <>
             <p className="font-semibold">Post comment</p>
             <textarea
+              onChange={e => {
+                setComment(e.target.value);
+              }}
               className="bg-neutral-600 text-white rounded"
               required
               name="comment"
